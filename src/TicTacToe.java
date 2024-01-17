a3f8434ee734a5c6739d54970c7f0b1da13105db
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-17 12:37:15 +0100

    TicTacToe: create public display method

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TicTacToe {
     final int size = 3;
     Cell[][] board = new Cell[size][size];
@@ -15,5 +17,9 @@ public class TicTacToe {
         for (int i = 0; i < size; i++) {
             board[i] = buildOneRow();
         }
+    }
+
+    public void display() {
+        System.out.println(Arrays.deepToString(board));
     }
 }
