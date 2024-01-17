5fd3c6a0db52fb9356f1e6c5a672971fab70645a
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-17 12:34:16 +0100

    TicTacToe: create buildOneRow method

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,13 @@
 public class TicTacToe {
     final int size = 3;
     Cell[][] board = new Cell[size][size];
+
+    private Cell[] buildOneRow() {
+        Cell[] row = new Cell[size];
+
+        for (int i = 0; i < size; i++) {
+            row[i] = new Cell();
+        }
+        return row;
+    }
 }
