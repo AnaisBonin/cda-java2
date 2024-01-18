ca0ad848dc0e3e3642e863a1798b00fcb6a2c301
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-18 10:52:20 +0100

    Tic Tac Toe: added comments and add cell availability condition

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class TicTacToe {
     final int size = 3;
     Player player = new Player();
+
+    //
+    // SET GAME BOARD AND SET BOARD ACCORDING TO SIZE
+    //
     Cell[][] board = new Cell[size][size];
 
     private Cell[] buildOneRow() {
@@ -25,6 +29,14 @@ public class TicTacToe {
         getBoard();
     }
 
+
+    //
+    // TICTACTOE METHODS
+    //
+
+    //
+    // Display visually game grid - size  * size
+    //
     public void display() {
         String endRow = "|";
         StringBuilder horizontalBorder = new StringBuilder();
@@ -42,19 +54,25 @@ public class TicTacToe {
         }
     }
 
+
+    //
+    // Check target chosen by Player: the target needs to be within the board size
+    //
+
     private boolean isTargetValid(int target) {
         return (target <= size && target > 0);
     }
 
     Scanner scanner = new Scanner(System.in);
+
     private int getPlayerEntry(String target) {
         int result;
 
         try {
             System.out.println("which " + target + " do you want to target?");
-            result = Integer.parseInt( scanner.nextLine());
+            result = Integer.parseInt(scanner.nextLine());
 
-            if (!isTargetValid(result)){
+            if (!isTargetValid(result)) {
                 System.out.println("Invalid - you must choose a number between 1 and " + size);
                 return getPlayerEntry(target);
             }
@@ -65,11 +83,29 @@ public class TicTacToe {
         return result;
     }
 
+    //
+    // Check if targeted cell is available
+    //
+
+    private boolean isCellAvailable(int[] move) {
+        if (move[0] == 1) {
+            System.out.println("this cell is not available, select another cell");
+            return false;
+        } else {
+            System.out.println("cell valid");
+            return true;
+        }
+    }
+
+    // Finale public method return player move
+
     public int[] getMoveFromPlayer() {
         int[] move = new int[2];
 
         move[0] = getPlayerEntry("Column");
         move[1] = getPlayerEntry("Row");
+
+        if (!isCellAvailable(move)) return getMoveFromPlayer();
 
         System.out.println("You picked: " + Arrays.toString(move));
         return move;
