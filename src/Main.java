06b74d84aca777b14839ff30cbd19f173762906b
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-18 10:16:13 +0100

    Player : add scanner and recursive method to getRepresentation;

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
+        Player player = new Player();
+
         ticTacToe.display();
         ticTacToe.getMoveFromPlayer();
+        player.getRepresentation();
+
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Player {
     String representation;
 
     public String getRepresentation() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Who do you want to play ? X or Y ? Press the X or Y key and Enter");
+
+        String playerRepresentation = scanner.nextLine();
+
+        switch (playerRepresentation) {
+            case "X":
+                representation = "|  X";
+                break;
+            case "Y":
+                representation = "|  Y";
+                break;
+            default:
+                System.out.println("Your entry is not valid, try again");
+                return getRepresentation();
+        }
+        System.out.println("Great! You are playing " + representation);
         return representation;
     }
 }
